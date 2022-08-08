490b89870bee94cf0b2997f99de7335b4ab1f5cf
CH217860 <sandip.samal@childrens.harvard.edu>
2022-08-08 14:20:15 -0400

    remove model from packages

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='dev@babyMRI.org',
     url='https://github.com/FNNDSC/pl-fpmmid',
     py_modules=['fpmmid'],
-    packages   = ['model','scripts.run','scripts.run.modules'],
+    packages   = ['scripts.run','scripts.run.modules'],
     install_requires=['chrisapp'],
     test_suite       = 'nose.collector',
     tests_require    = ['nose'],
