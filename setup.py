dfd1fe027af143dac051aa7a896e562a2d3a125b
CH217860 <sandip.samal@childrens.harvard.edu>
2022-08-22 15:07:21 -0400

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='fpmmid',
-    version='1.1.3',
+    version='1.1.4',
     description='A ChRIS DS plugin that wraps around FPMMID',
     author='FNNDSC',
     author_email='dev@babyMRI.org',
