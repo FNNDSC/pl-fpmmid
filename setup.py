28a7456a9aa518a5a0aa7d451068205b846bffbe
CH217860 <sandip.samal@childrens.harvard.edu>
2022-09-07 11:37:23 -0400

    update memory requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='fpmmid',
-    version='1.1.5',
+    version='1.1.6',
     description='A ChRIS DS plugin that wraps around FPMMID',
     author='FNNDSC',
     author_email='dev@babyMRI.org',
